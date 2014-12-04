2374e4e7f726874edc1c6e6172b69a9b66386e9e
Jan Sorensen <jas@praqma.net>
2014-12-04 21:34:39 +0100

    Shutdown HostConnection in tests

--- src/test/groovy/net/praqma/gradle/docker/test/ProjectTestCase.groovy
+++ src/test/groovy/net/praqma/gradle/docker/test/ProjectTestCase.groovy
@@ -1,17 +1,24 @@
 package net.praqma.gradle.docker.test
 
+import net.praqma.gradle.docker.DockerAppliance
 import net.praqma.gradle.docker.DockerContainer
-import net.praqma.gradle.docker.jobs.ContainerJob;
-import net.praqma.gradle.docker.jobs.JobScheduler;
+import net.praqma.gradle.docker.jobs.ApplianceJob
+import net.praqma.gradle.docker.jobs.ContainerJob
+import net.praqma.gradle.docker.jobs.JobScheduler
 
 import org.gradle.api.Project
 import org.gradle.testfixtures.ProjectBuilder
+import org.junit.After
 
 import com.google.common.io.Files
 
 class ProjectTestCase {
 
+	static String BUSYBOX_IMAGE = 'busybox:buildroot-2014.02'
+
 	static private Random rand = new Random()
+
+	private List<Project> projectsCreated = []
 
 	Project newRootProject(boolean withPlugin = true) {
 		Project project = ProjectBuilder.builder()
@@ -21,6 +28,7 @@ class ProjectTestCase {
 		if (withPlugin) {
 			project.with { apply plugin: 'net.praqma.docker' }
 		}
+		projectsCreated << project
 		project
 	}
 
@@ -36,9 +44,7 @@ class ProjectTestCase {
 
 	Project projectWithDocker(Closure c) {
 		Project project = newRootProject()
-		project.with {
-			docker.with(c)
-		}
+		project.with { docker.with(c) }
 		project
 	}
 
@@ -57,7 +63,15 @@ class ProjectTestCase {
 	void remove(DockerContainer c) {
 		JobScheduler.execute(ContainerJob.Remove, c)
 	}
-	
+
+	void start(DockerAppliance app) {
+		JobScheduler.execute(ApplianceJob.Start, app)
+	}
+
+	void stop(DockerAppliance app) {
+		JobScheduler.execute(ApplianceJob.Stop, app)
+	}
+
 	void waitFor(int ms, Closure cond) {
 		while (!cond()) {
 			if (ms > 0) {
@@ -65,6 +79,15 @@ class ProjectTestCase {
 				ms -= 50
 			} else {
 				assert cond() : "Condition not reached in $ms milleseconds"
+			}
+		}
+	}
+
+	@After
+	void tearDown() {
+		projectsCreated.each {
+			if (it.hasProperty('docker')) {
+				it.docker.connection.shutdown()
 			}
 		}
 	}
