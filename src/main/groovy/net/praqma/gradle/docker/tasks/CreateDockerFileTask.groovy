5fdd594297311eb6f4fabc0e3cefc215c893f2c4
Jan Sorensen <jas@praqma.net>
2015-02-03 10:54:39 +0100

    Fix sample app

---
+++ src/main/groovy/net/praqma/gradle/docker/tasks/CreateDockerFileTask.groovy
@@ -0,0 +1,41 @@
+package net.praqma.gradle.docker.tasks
+
+import groovy.transform.CompileStatic
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.Input
+import org.gradle.api.tasks.OutputFile
+import org.gradle.api.tasks.TaskAction
+
+@CompileStatic
+class CreateDockerFileTask extends DefaultTask {
+
+	private StringBuffer buffer = new StringBuffer()
+
+	@OutputFile	
+	File file
+	
+	@TaskAction
+	void createFile() {
+		file.text = text
+	}
+	
+	@Input
+	String getText() {
+		return buffer.toString ()
+	}
+	
+ 	void appendLine(String instruction, String line) {
+		buffer << instruction << " " << line << "\n"
+	}
+
+	void appendLine(String instruction, String ...line) {
+		buffer << instruction << " " << toJsonArray(line) << "\n"
+	}
+
+	private String toJsonArray(String[] ary) {
+		new groovy.json.JsonBuilder(ary).toString()
+	}
+
+
+}
